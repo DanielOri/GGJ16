a9242a139e948e315bc4057393599c72d6acf339
Daniel Ribeiro <danielribeiro.new@gmail.com>
2016-01-30 20:10:52 -0300

    With FUCKING ITWEENS
    Goddamit, Renee

--- Assets/Scripts/Screens/Screen.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class Screen : MonoBehaviour {
-
-    public abstract void InitScreen();
-    public abstract void ChangeScreen();
-    public abstract void KillScreen();
-}
---
+++ Assets/TestRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRotation : MonoBehaviour {
+
+    Hashtable hash = new Hashtable ();
+
+    void Awake () {
+        hash.Add ("y", 90f);
+        hash.Add ("time", 2);
+        hash.Add ("looptype", iTween.EaseType.easeOutSine);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+}
