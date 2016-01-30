2c15dd4c26dc3ff703fe4096f7fbde94b89ba6d5
Daniel Ribeiro <danielribeiro.new@gmail.com>
2016-01-30 09:51:04 -0300

    Third commit
    This time for real

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+
+    bool isGround;
+    public Transform checkerOrigin;
+    public float checkerRadius;
+    public LayerMask whatIsGround;
+
+    Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update () {
+        if (Input.GetButtonDown("Jump") && isGround) {
+            rb2d.velocity = new Vector2 (rb2d.velocity.x, jumpForce);
+
+        }
+    }
+
+	void FixedUpdate () {
+        rb2d.velocity = new Vector2 (speed * Input.GetAxisRaw("Horizontal"), rb2d.velocity.y);
+        isGround = Physics2D.OverlapCircle (checkerOrigin.position, checkerRadius, whatIsGround);
+    }
+}
