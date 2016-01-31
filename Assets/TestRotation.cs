5bb1b5915281fd45c48dda4e5eaeb1e13ad375ae
Daniel Ribeiro <danielribeiro.new@gmail.com>
2016-01-30 23:13:24 -0300

    Whatever 3.0
    The more whatever we got, the more the commits we have

--- Assets/TestRotation.cs
+++ Assets/TestRotation.cs
@@ -1,25 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
-public class TestRotation : MonoBehaviour {
+public class TestRotation : MonoBehaviour, IDragHandler {
 
-    Hashtable hash = new Hashtable ();
+    Hashtable hashRight = new Hashtable ();
+    Hashtable hashLeft = new Hashtable ();
 
     void Awake () {
-        hash.Add ("y", 90f);
-        hash.Add ("time", 2);
-        hash.Add ("looptype", iTween.EaseType.easeOutSine);
+        hashRight.Add ("name", "CameraRight");
+        hashRight.Add ("y", 90f);
+        hashRight.Add ("time", 0.5f);
+        hashRight.Add ("looptype", iTween.EaseType.easeOutSine);
+
+        hashLeft.Add ("name", "CameraLeft");
+        hashLeft.Add ("y", -90f);
+        hashLeft.Add ("time", 0.5f);
+        hashLeft.Add ("looptype", iTween.EaseType.easeOutSine);
     }
 
-	// Use this for initialization
-	void Start () {
-	
+    public void OnDrag (PointerEventData eventData) {
+        Vector2 dir = Vector2.zero;
+        dir = eventData.delta.normalized;
+        Debug.Log (dir);
+        if (!Camera.main.GetComponent<iTween>()) {
+            if (dir.x < -.1f) {
+                iTween.RotateAdd (Camera.main.gameObject, hashRight);
+            }
+            if (dir.x > .1f) {
+                iTween.RotateAdd (Camera.main.gameObject, hashLeft);
+            }
+        }
         
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
 
     }
 }
