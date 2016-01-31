34ab3281e173475fdb704418595809b4873f53c5
Daniel Ribeiro <danielribeiro.new@gmail.com>
2016-01-31 04:37:37 -0300

    Menus

---
+++ Assets/MenuController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour, IDragHandler {
+
+    Hashtable hashRight = new Hashtable ();
+    Hashtable hashLeft = new Hashtable ();
+
+    public bool isClassMenuOpen;
+    public GameObject auxiliarCanvas, classesMenu;
+
+    void Awake () {
+        hashRight.Add ("name", "CameraRight");
+        hashRight.Add ("y", 120f);
+        hashRight.Add ("time", 0.5f);
+        hashRight.Add ("looptype", iTween.EaseType.easeOutSine);
+
+        hashLeft.Add ("name", "CameraLeft");
+        hashLeft.Add ("y", -120f);
+        hashLeft.Add ("time", 0.5f);
+        hashLeft.Add ("looptype", iTween.EaseType.easeOutSine);
+    }
+
+    public void OnDrag (PointerEventData eventData) {
+        if (!isClassMenuOpen) {
+            Vector2 dir = Vector2.zero;
+            dir = eventData.delta.normalized;
+
+            if (!Camera.main.GetComponent<iTween> ()) {
+                if (dir.x < -.1f) {
+                    iTween.RotateAdd (Camera.main.gameObject, hashRight);
+                }
+                if (dir.x > .1f) {
+                    iTween.RotateAdd (Camera.main.gameObject, hashLeft);
+                }
+            }
+        }
+    }
+
+    public void OpenClassMenu (int classID) {
+        if (classID == 1) {
+            isClassMenuOpen = true;
+            auxiliarCanvas.SetActive (false);
+            classesMenu.SetActive (true);
+        }
+    }
+}
