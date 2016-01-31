77bfc2ff78a5e2630a33ff21c08639eef0b3fc8f
reneesalles <reneesalles@hotmail.com>
2016-01-30 23:29:40 -0300

    Commit

--- Assets/Scripts/Enums/EnumUtils.cs
+++ Assets/Scripts/Enums/EnumUtils.cs
@@ -2,6 +2,69 @@
 using System.Collections;
 
 public class EnumUtils {
+    #region MagicClasses
+    public enum MagicClasses {
+        HERBOLOGY,
+        MAGIC_ARTIFACTS,
+        ALCHEMY
+    }
+
+    public enum Herbology {
+        PLAGUE_CONTROL,
+        MAGIC_PLANT_CARE
+    }
+
+        public enum Plague_Control {
+            COLOR
+        }
+        public enum Magic_Plant_Care {
+            SHAPE
+        }
+
+    public enum Magic_Artifacts {
+        AMULETS,
+        RUNES,
+        WANDS
+    }
+
+        public enum Amulets {
+            SHAPE
+        }
+
+        public enum RUNES {
+            GLYPHS
+        }
+
+        public enum Wands {
+            COLOR,
+            MATERIAL
+        }
+
+    public enum Alchemy {
+        POTIONS,
+        GEMS,
+        TRANSFIGURATION
+    }
+
+    public enum Potions {
+        COLOR,
+        SHAPE
+    }
+
+    public enum Gems {
+        COLOR,
+        SHAPE
+    }
+
+    public enum Transfiguration {
+        COLOR,
+        SHAPE,
+        GLYPHS
+    }
+
+    #endregion
+
+    #region LevelData
     public static PlayerPrefsKeys NEXTLEVEL = PlayerPrefsKeys.NEXTLEVEL;
     public static PlayerPrefsKeys LOADLEVELMODE = PlayerPrefsKeys.LOADLEVELMODE;
 
@@ -9,6 +72,7 @@ public class EnumUtils {
         NEXTLEVEL,
         LOADLEVELMODE
     }
+    #endregion
 
     #region GameFlow
     public enum EScreenState {
